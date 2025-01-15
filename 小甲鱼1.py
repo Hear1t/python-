0fc1060ea5708737c7cfc772f83de6b2557ee0dd
zebin li <139696417+Hear1t@users.noreply.github.com>
2025-01-15 23:52:54 +0800

    Add files via upload

---
+++ 小甲鱼1.py
@@ -0,0 +1,17 @@
+'''temp=input("number")
+guess=int(temp)
+if guess==8:
+    print("correct")
+else:
+    print("wrong")
+print("end")'''
+'''x=3
+y=5
+x,y=y,x
+print(x,y)'''
+impor
+count=3
+while(count):
+    print("i love fishc\nyes")
+    count=count-1
+    break
